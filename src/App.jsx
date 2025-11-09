70c3b700aa4f40e9118dd599aee1a0d73dcf5b55
Ali <aliazadigithub@gmail.com>
2025-11-09 18:16:03 +0330

    Initial files

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './Pages/Login'
+import Landing from './Pages/Landing'
+import { useRoutes } from 'react-router-dom'
 
+const routes = [
+  {path:"/" ,element:<Landing/>},
+  {path:"/Login" ,element:<Login/>},
+]
 function App() {
+  const router = useRoutes(routes);
 
   return (
     <div>
-      
+
+      {router}
     </div>
+   
 
   )
 }
